e320d1f1f869a75cf6454021801ed4e6a3ef0e3f
Jovanni D <80374076+superg3m@users.noreply.github.com>
2025-06-16 14:50:01 -0400

    remove the map for input states

--- IOD.cpp
+++ IOD.cpp
@@ -1,6 +1,6 @@
 #include <IOD.hpp>
 
-std::unordered_map<IOD_InputCode, IOD_InputState> IOD::input_state;
+IOD_InputState IOD::input_state[256] = {IOD_InputState::UP};
 std::unordered_map<std::string, IOD_Profile*> IOD::profiles;
 float IOD::mouse_x = 0.0f;
 float IOD::mouse_y = 0.0f;
